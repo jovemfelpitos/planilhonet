9fd38f9277faaea750ae2687a87da540426f8548
Felps <109309830+jovemfelpitos@users.noreply.github.com>
2025-07-08 12:20:17 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,14 @@
 <?php
-
-if(!isset($_SESSION)) {
+if (!isset($_SESSION)) {
     session_start();
 }
 
+// Limpa todas as variáveis da sessão
+session_unset();
+
+// Destroi a sessão
 session_destroy();
 
+// Redireciona para a página de login
 header("Location: index.php");
+exit();
